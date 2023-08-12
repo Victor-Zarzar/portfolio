830567b3a23ad993c6154f4e9d0bf4a4ce2274d8
victorzarzar <victorzarzar58@gmail.com>
2023-08-12 11:20:53 -0300

    updates

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -6,13 +6,13 @@ export function Experience(){
     return(
         <div className="experience">
           <SectionTitle text="Experience"/>
-          <p>Software Developer, in companys XLabs Security.</p>
+          <p>Software Developer, in company XLabs Security.</p>
           <p>March 2023 - Present</p>
           <div className="experience-time">
-            <Skill image="/react.png" measure={2} years="6 months"/>
-            <Skill image="/js.png" measure={3} years="6 months"/>
-            <Skill image="/flutter.png" measure={1} years="4 months"/>
-            <Skill image="/ts.png" measure={4} years="1 month"/>           
+            <Skill image="/react.png" measure={2} years=""/>
+            <Skill image="/js.png" measure={3} years=""/>
+            <Skill image="/flutter.png" measure={1} years=""/>
+            <Skill image="/ts.png" measure={4} years=""/>           
           </div>
         </div>
     )
